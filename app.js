7f71068a12c058e579982d0d8b45d304f8316290
Juliana Sousa <139198662+julianasdev@users.noreply.github.com>
2024-09-07 23:06:31 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+function pesquisar() {
+        // Obtém a seção HTML onde os resultados serão exibidos.
+        let section = document.getElementById("resultados-pesquisa");
+
+        let campoPesquisa = document.getElementById("campo-pesquisa").value 
+        
+        //se o campo pesquisa for uma string sem nada
+        if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome do atleta</p>"
+        return
+        }
+        
+        campoPesquisa = campoPesquisa.toLowerCase()
+        // Inicializa uma string para armazenar os resultados da pesquisa.
+        let resultados = "";
+        let titulo = "";
+        let descricao = "";
+        let  tags = "";
+
+        // Itera sobre cada dado da lista de dados e constrói o HTML para cada resultado.
+        for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase() 
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+           // cria um novo elemento
+          resultados += `
+          <div class="item-resultado">
+          <h2>
+          <a href="#" target="_blank">${dado.titulo}</a>
+          </h2>
+          <p class="descricao-meta">${dado.descricao}</p>
+          <a href="${dado.link}" target="_blank">Mais informações</a>
+          </div>
+          `;
+        }
+    }
+
+    if (!resultados) {
+      resultados = "<p>Nada foi encontrado</p>"
+    }
+
+    //Atribui os resultados gerados à seção HTML
+    section.innerHTML = resultados
+  }
---
+++ dados.js
@@ -0,0 +1,32 @@
+let dados = [
+    {
+      titulo: "Zé Maria",
+      descricao: "Jogar com raça era seu lema e, se precisasse, dava o sangue pelo time. E, apesar de não ter conseguido um título na Copa do Mundo de 1974, deu muitas alegrias à torcida corintiana. Na final do Paulistão de 1979, mesmo com um corte na cabeça e com a camisa manchada de sangue, Zé Maria permaneceu em campo até o apito final e o título do Corinthians.",
+      link: "https://pt.wikipedia.org/wiki/Jos%C3%A9_Maria_Rodrigues_Alvess",
+      tags: "raça camisa sangue"
+    },
+    {
+      titulo: "Sócrates",
+      descricao: "Além de suas habilidades com a bola, ele era conhecido por sua inteligência e liderança, promovendo o debate e a participação de todos os membros do time nas decisões. Sua visão de futebol como um esporte coletivo e sua luta por uma sociedade mais justa transformaram-no em um dos maiores ídolos do Corinthians e um símbolo de resistência e esperança para muitos brasileiros.",
+      link: "https://pt.wikipedia.org/wiki/S%C3%B3crates_Brasileiro_Sampaio_de_Souza_Vieira_de_Oliveira",
+      tags:"magrão doutor magrao "
+    },
+    {
+      titulo: "Casagrande",
+      descricao: "Centroavante de grande talento, Casagrande foi um dos principais nomes da Democracia Corintiana. Sua luta contra a dependência química e seu ativismo social o transformaram em um símbolo de superação e esperança para muitos brasileiros.",
+      link: "https://pt.wikipedia.org/wiki.org/Casagrande",
+      tags:"casa casão casao"
+    },
+    {
+      titulo: "Wladimir",
+      descricao: "Um dos pilares da Democracia Corintiana, Wladimir era conhecido por sua inteligência tática e sua visão de jogo. Sua participação no movimento foi fundamental para o sucesso da iniciativa.",
+      link: "https://pt.wikipedia.org/wiki/Wladimir_Belini",
+      tags:"lateral esquerdo zumbi "
+    },
+    {
+      titulo: "Neto",
+      descricao: "Meio-campista talentoso e de personalidade forte, Neto era conhecido por suas opiniões polêmicas e por sua liderança dentro do grupo. Sua luta por justiça social o tornou um ícone para muitos brasileiros.",
+      link: "https://pt.wikipedia.org/wiki/Neto_(futebolista)",
+      tags:"craque xodó xodo"
+    }
+  ];
